930aca487d48cc08d1a037f36c14043cd1a34a84
Pete Mettaprasert <collegeforpete@gmail.com>
2023-10-20 21:39:12 -0700

    The commit adds three new tests to the GeometricUtilitiesTest class to ensure the correct functionality of the 'extrude' method in different scenarios (positive, negative, or zero extension).

--- litiengine/src/test/java/de/gurkenlabs/litiengine/util/geom/GeometricUtilitiesTest.java
+++ litiengine/src/test/java/de/gurkenlabs/litiengine/util/geom/GeometricUtilitiesTest.java
@@ -9,7 +9,78 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GeometricUtilitiesTest {
+  @Test
+  void testExtrude_PositiveExtension() {
+    // Arrange
 
+    // Create an original Rectangle2D object with specified dimensions.
+    Rectangle2D original = new Rectangle2D.Double(10, 10, 20, 20);
+    // Set the extension amount. Positive extension should expand the rectangle.
+    double extension = 5;
+
+    // Act
+
+    // Call the extrude method from the GeometricUtilities class.
+    // This should return a new rectangle with expanded dimensions.
+    Rectangle2D extruded = GeometricUtilities.extrude(original, extension);
+
+    // Assert
+
+    // Validate each dimension of the extruded rectangle.
+    // The X and Y should have been reduced by the extension amount,
+    // and the width and height should have increased by twice the extension amount.
+    assertEquals(5, extruded.getX());
+    assertEquals(5, extruded.getY());
+    assertEquals(30, extruded.getWidth());
+    assertEquals(30, extruded.getHeight());
+  }
+
+  @Test
+  void testExtrude_NegativeExtension() {
+    // Arrange
+
+    // Create another original Rectangle2D object.
+    Rectangle2D original = new Rectangle2D.Double(10, 10, 20, 20);
+    // Set a negative extension amount. This should shrink the rectangle.
+    double extension = -5;
+
+    // Act
+
+    // Call the extrude method to get the new rectangle dimensions.
+    Rectangle2D extruded = GeometricUtilities.extrude(original, extension);
+
+    // Assert
+
+    // Verify the dimensions. For a negative extension, the X and Y should increase,
+    // and the width and height should decrease.
+    assertEquals(15, extruded.getX());
+    assertEquals(15, extruded.getY());
+    assertEquals(10, extruded.getWidth());
+    assertEquals(10, extruded.getHeight());
+  }
+
+  @Test
+  void testExtrude_ZeroExtension() {
+    // Arrange
+
+    // Create another original Rectangle2D object.
+    Rectangle2D original = new Rectangle2D.Double(10, 10, 20, 20);
+    // Set extension as zero. This should return a rectangle with the same dimensions.
+    double extension = 0;
+
+    // Act
+
+    // Call the extrude method to get the new rectangle dimensions.
+    Rectangle2D extruded = GeometricUtilities.extrude(original, extension);
+
+    // Assert
+
+    // Verify the dimensions. With zero extension, all dimensions should remain unchanged.
+    assertEquals(10, extruded.getX());
+    assertEquals(10, extruded.getY());
+    assertEquals(20, extruded.getWidth());
+    assertEquals(20, extruded.getHeight());
+  }
 
 
 }
