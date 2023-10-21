30113cc4862a1ee6083be0f0f4d36faceec72774
Pete Mettaprasert <collegeforpete@gmail.com>
2023-10-20 21:32:00 -0700

    Add new unit test: GeometricUtilitiesTest

---
+++ litiengine/src/test/java/de/gurkenlabs/litiengine/util/geom/GeometricUtilitiesTest.java
@@ -0,0 +1,15 @@
+package de.gurkenlabs.litiengine.util.geom;
+
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GeometricUtilitiesTest {
+
+
+
+}
