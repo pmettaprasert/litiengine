fdaa88e5d837b5b3d65506391d6a272cffc59ed5
Pete Mettaprasert <collegeforpete@gmail.com>
2023-11-22 13:44:51 -0800

    Add tests for align object getValue method. Still need to add comments though
    Adding three new tests in the AlignTests class. These tests cover the getValue method of the align object when this is set to right, left, and center align. The tests ensure that the returned value is correctly calculated from the product of the width and the portion. Increased the Align class from 87% method percentage to 100% and line 95% to 100%.

--- litiengine/src/test/java/de/gurkenlabs/litiengine/AlignTests.java
+++ litiengine/src/test/java/de/gurkenlabs/litiengine/AlignTests.java
@@ -48,4 +48,44 @@ class AlignTests {
     // assert
     assertEquals(0.5, outPointLocation, 0.001); // clamp(0.75, 0, 0.5)
   }
+
+  @Test
+  void getValue_RightAlign() {
+    // Arrange
+    float width = 10.0f;
+
+    // Act
+    float value = alignObject.getValue(width);
+
+    // Assert
+    assertEquals(10.0f, value, 0.001); // Assert that the value is width * portion (10.0 * 1.0)
+  }
+
+  @Test
+  void getValue_LeftAlign() {
+    // Arrange
+    alignObject = Align.LEFT; // portion = 0
+    float width = 10.0f;
+
+    // Act
+    float value = alignObject.getValue(width);
+
+    // Assert
+    assertEquals(0.0f, value, 0.001); // Assert that the value is width * portion (10.0 * 0.0)
+  }
+
+  @Test
+  void getValue_CenterAlign() {
+    // Arrange
+    alignObject = Align.CENTER; // portion = 0.5
+    float width = 10.0f;
+
+    // Act
+    float value = alignObject.getValue(width);
+
+    // Assert
+    assertEquals(5.0f, value, 0.001); // Assert that the value is width * portion (10.0 * 0.5)
+  }
+
+
 }
