b9c94a8d18c39df2102a9d33cf53975d83926829
erinjinz <zjin1@seattleu.edu>
2023-11-27 22:45:43 -0800

    Integration Test Added for Gamepad Tests.

--- litiengine/src/test/java/de/gurkenlabs/litiengine/input/GamepadTests.java
+++ litiengine/src/test/java/de/gurkenlabs/litiengine/input/GamepadTests.java
@@ -1,11 +1,14 @@
 package de.gurkenlabs.litiengine.input;
-
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.input.Gamepad;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
 
 public class GamepadTests {
-
   @Test
   public void testButtons() {
     String a = Gamepad.Xbox.A;
@@ -14,4 +17,28 @@ public class GamepadTests {
     assertEquals("0", a);
     assertEquals(0.0f, off);
   }
+
+  @BeforeAll
+  static void setup() {
+    // Initialize the game environment
+    Game.init();
+  }
+
+  @Test
+  void testGamepadInteraction() {
+    // Create and initialize a Gamepad instance using mock
+
+    // Simulate gamepad input
+    // For example, simulate a button press
+
+    // Verify that the game responds correctly to the input
+    // i.e., assert that a character moves or an action occurs
+  }
+
+  @AfterAll
+  static void tearDown() {
+    // Clean up and terminate the game
+    // terminate is private. cannot be accessed from outside package
+  }
+
 }
